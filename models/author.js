6010c64ca167293d0f0481b88296561aace0e85a
MaiHoa <hoa.le22@student.passerellesnumeriques.org>
2021-12-27 16:14:00 +0700

    CRUD Author

--- models/author.js
+++ models/author.js
@@ -40,6 +40,14 @@ AuthorSchema.virtual('lifespan').get(function() {
   return lifetime_string;
 });
 
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); //format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); //format 'YYYY-MM-DD'
+});
+
 // Virtual for author's URL
 AuthorSchema
 .virtual('url')
