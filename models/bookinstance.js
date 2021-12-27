8c1a83c5377d10a15f75aa2a349099828887b68e
MaiHoa <hoa.le22@student.passerellesnumeriques.org>
2021-12-28 01:59:44 +0700

    Finish CRUD book instance

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,22 +1,20 @@
 var mongoose = require('mongoose');
-const { DateTime } = require("luxon");
+const { DateTime } = require("luxon");  //for date handling
 
 var Schema = mongoose.Schema;
 
-var BookInstanceSchema = new Schema(
-  {
-    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+var BookInstanceSchema = new Schema({
+    book: { type: Schema.ObjectId, ref: 'Book', required: true }, // Reference to the associated book.
     imprint: {type: String, required: true},
-    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
-    due_back: {type: Date, default: Date.now}
-  }
-);
+    status: {type: String, required: true, enum:['Available', 'Maintenance', 'Loaned', 'Reserved'], default:'Maintenance'},
+    due_back: { type: Date, default: Date.now },
+});
 
-// Virtual for bookinstance's URL
+// Virtual for this bookinstance object's URL.
 BookInstanceSchema
 .virtual('url')
 .get(function () {
-  return '/catalog/bookinstance/' + this._id;
+  return '/catalog/bookinstance/'+this._id;
 });
 
 BookInstanceSchema
@@ -25,6 +23,12 @@ BookInstanceSchema
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
+BookInstanceSchema
+.virtual('due_back_yyyy_mm_dd')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate(); //format 'YYYY-MM-DD'
+});
 
-//Export model
+
+// Export model.
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
