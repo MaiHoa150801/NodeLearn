6ef92329fab0cf492d711b7857143a807ac9f1f6
tuannguyen22 <tuannguyenit263@gmail.com>
2022-01-26 09:36:26 +0700

    Cookie

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,19 @@
 var app = require("express")();
 var bodyParser = require("body-parser");
-const Swal = require("sweetalert2");
 app.use(bodyParser.json());
+var cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 exports.dashboard_get = function (req, res) {
-  res.render("dashboard");
+  if (req.cookies.check) {
+    res.render("dashboard");
+  } else {
+    res.redirect("/login-admin");
+  }
 };
 
 exports.logout_admin = function (req, res) {
+  res.clearCookie("check");
   res.redirect("/login-admin");
 };
 
@@ -20,30 +26,9 @@ exports.admin_login_post = function (req, res) {
   var email = body.email;
   var password = body.password;
   if (email === "coolteamadmin@gmail.com" && password === "123456") {
-    const Toast = Swal.mixin({
-      toast: true,
-      position: "top-end",
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener("mouseenter", Swal.stopTimer);
-        toast.addEventListener("mouseleave", Swal.resumeTimer);
-      },
-    });
-
-    Toast.fire({
-      icon: "success",
-      title: "Signed in successfully",
-    });
-    res.render("dashboard");
+    res.cookie("check", true);
+    res.redirect("/admin");
   } else {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Something went wrong!",
-      footer: '<a href="">Why do I have this issue?</a>',
-    });
     res.redirect("/login-admin");
   }
 };
