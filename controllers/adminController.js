4cd458402830cde29299eaa16dbda719abfa9420
tuannguyen22 <tuannguyenit263@gmail.com>
2022-01-26 10:33:56 +0700

    Show Data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,12 +4,45 @@ app.use(bodyParser.json());
 var cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
+var Book = require("../models/book");
+var Author = require("../models/author");
+var Genre = require("../models/genre");
+var BookInstance = require("../models/bookinstance");
+const { body, validationResult } = require("express-validator");
+
+var async = require("async");
+
 exports.dashboard_get = function (req, res) {
-  if (req.cookies.check) {
-    res.render("dashboard");
-  } else {
-    res.redirect("/login-admin");
-  }
+  async.parallel(
+    {
+      book_count: function (callback) {
+        Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      book_instance_count: function (callback) {
+        BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count: function (callback) {
+        BookInstance.countDocuments({ status: "Available" }, callback);
+      },
+      author_count: function (callback) {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: function (callback) {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    function (err, results) {
+      if (req.cookies.check) {
+        res.render("dashboard", {
+          title: "Local Library Home",
+          error: err,
+          data: results,
+        });
+      } else {
+        res.redirect("/login-admin");
+      }
+    }
+  );
 };
 
 exports.logout_admin = function (req, res) {
